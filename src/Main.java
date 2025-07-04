66bc3f932f5cf8e3a895fa3aa98d31a5a06cd6c5
Paramjot Singh <psingh@catalystone.com>
2025-07-04 23:57:35 +0530

    java-multithreading: getting started

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    System.out.println("We are now in thread: " + Thread.currentThread().getName());
+                    System.out.println("Current thread priority: " + Thread.currentThread().getPriority());
+                }
+        );
+
+        thread.setName("New worker thread");
+
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println("We are now in thread: " + Thread.currentThread().getName());
+        System.out.println("Starting thread: " + thread.getName());
+        thread.start();
+        try {
+            thread.join(); // Wait for the thread to finish
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
