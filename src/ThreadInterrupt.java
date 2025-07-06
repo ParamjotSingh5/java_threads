59d18929d27a8d16d286c26fbf7c6424e7d8d3c1
ParamjotSingh5 <Paramjotdhiman5@gmail.com>
2025-07-06 23:42:08 +0530

    Customise behaviour to interrupt cal on a worker thread from Runnable#run method.

---
+++ src/ThreadInterrupt.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+
+public class ThreadInterrupt {
+
+    public static void main(String[] args) {
+
+        Thread blockingThread = new Thread(new BlockingThread());
+        blockingThread.start();
+
+        blockingThread.interrupt();
+
+    }
+
+    private static class BlockingThread implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(5000000); // Simulate a long-running task
+            } catch (InterruptedException e) {
+                System.out.println("Blocking thread interrupted: " + e.getMessage());
+            }
+        }
+    }
+}
