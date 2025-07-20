f26fcb1eb217b542f9b4c56463049addd0ab2459
ParamjotSingh5 <Paramjotdhiman5@gmail.com>
2025-07-20 19:59:25 +0530

    Sanity check, inventory count is always 0 in this case.

---
+++ src/ShareAcrossThreads/Main.java
@@ -0,0 +1,71 @@
+package ShareAcrossThreads;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        InventoryCounter inventoryCounter = new InventoryCounter(0);
+
+
+        Thread incrementingThread = new IncrementingThread(inventoryCounter);
+        Thread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+        incrementingThread.join();
+
+        decrementingThread.start();
+        decrementingThread.join();
+
+        System.out.println("Final inventory count: " + inventoryCounter.getCount());
+    }
+
+    public static  class DecrementingThread extends Thread{
+        private final InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run(){
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    public static class IncrementingThread extends Thread {
+
+        private final InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run(){
+            for(int i = 0; i < 10000; i++){
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    public static class InventoryCounter {
+        private int count;
+
+        public InventoryCounter(int initialCount) {
+            this.count = initialCount;
+        }
+
+        public void increment() {
+            count++;
+        }
+
+        public void decrement() {
+            count--;
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+}
