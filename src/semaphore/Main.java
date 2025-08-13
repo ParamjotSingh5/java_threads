9aa2065033833c73552f3a9c759857b926c2e760
ParamjotSingh5 <Paramjotdhiman5@gmail.com>
2025-08-13 23:30:00 +0530

    Understaing purpose of semaphores and a producde/consumer pattern built out of semaphore feature.

--- src/semaphore/Main.java
+++ src/semaphore/Main.java
@@ -5,22 +5,28 @@ import java.util.concurrent.Semaphore;
 public class Main {
     Semaphore full = new Semaphore(0);
     Semaphore empty = new Semaphore(1);
-    int buffer = 0;
+    int buffer = 1;
 
     public static void main(String[] args) throws InterruptedException {
         Main main = new Main();
         Thread producer = new Thread(main::produce);
         Thread consumer = new Thread(main::consume);
 
-        producer.start();
         consumer.start();
+        producer.start();
     }
 
     private void produce() {
         while (true) {
             try{
                 System.out.println("Acquiring full semaphore");
-                full.acquire();
+                full.acquire();// In first iteration, this will block until the consumer has consumed an item
+                // This ensures that we do not produce more items than the buffer can hold
+                // In subsequent iterations, it will block until the consumer has consumed an item
+                // and released the full semaphore
+                // This allows the producer to produce an item only when there is space in the buffer
+                // This is a classic producer-consumer problem where the producer waits for the consumer to consume
+                // an item before producing a new one
                 System.out.println("Producing item, current buffer size: " + buffer);
                 buffer++;
                 System.out.println("Releasing empty semaphore");
