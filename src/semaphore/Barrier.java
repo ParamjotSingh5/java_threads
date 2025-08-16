84e04ad50d386a3da7c947fd2dd280825015c2b8
ParamjotSingh5 <Paramjotdhiman5@gmail.com>
2025-08-16 18:19:21 +0530

    Java semaphore, build a barrier that blocks access to section, until all the threads have completed part1 of the task, then any thread can move on to part2 of the task. By that mean, traversing from part1 of the task to part2 should be atomic, such that every thread should start part2 simantanously.

--- src/semaphore/Barrier.java
+++ src/semaphore/Barrier.java
@@ -1,4 +1,85 @@
 package semaphore;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Barrier {
+    private final int numberOfWorkers;
+    private final Semaphore semaphore = new Semaphore( 0);
+    private int counter = 0;
+    private final Lock lock = new ReentrantLock();
+
+    public Barrier(int numberOfWorkers) {
+        this.numberOfWorkers = numberOfWorkers;
+    }
+
+    public void waitForOthers() throws InterruptedException {
+        lock.lock();
+        boolean isLastWorker = false;
+        try {
+            counter++;
+
+            if (counter == numberOfWorkers) {
+                isLastWorker = true;
+            }
+        } finally {
+            lock.unlock();
+        }
+
+        if (isLastWorker) {
+            semaphore.release(numberOfWorkers - 1);
+            System.out.printf("All worker threads have completed part1 of the task, " +
+                    "released %s number of permits, blocking threads will continue now.", numberOfWorkers - 1);
+        } else {
+            semaphore.acquire();
+        }
+    }
+
+    public static class CoordinatedWorkRunner implements Runnable {
+        private final Barrier barrier;
+
+        public CoordinatedWorkRunner(Barrier barrier) {
+            this.barrier = barrier;
+        }
+
+        @Override
+        public void run() {
+            try {
+                task();
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted thread...");
+            }
+        }
+
+        private void task() throws InterruptedException {
+            // Performing Part 1
+            System.out.println(Thread.currentThread().getName()
+                    + " part 1 of the work is finished");
+
+            barrier.waitForOthers();
+
+            // Performing Part2
+            System.out.println(Thread.currentThread().getName()
+                    + " part 2 of the work is finished");
+        }
+    }
+
+    public static void main(String [] args) throws InterruptedException {
+        int numberOfThreads = 200; //or any number you'd like
+
+        List<Thread> threads = new ArrayList<>();
+
+        Barrier barrier = new Barrier(numberOfThreads);
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads.add(new Thread(new CoordinatedWorkRunner(barrier)));
+        }
+
+        for(Thread thread: threads) {
+            thread.start();
+        }
+    }
+
 }
