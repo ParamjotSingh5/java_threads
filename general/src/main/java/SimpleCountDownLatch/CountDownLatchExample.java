342bd0d26016fa4315d6fa8bf459fbb51c465aa7
Paramjot Singh <Paramjotdhiman5@gmail.com>
2025-08-30 23:56:34 +0530

    test: Define a test for SimpleCountDownLatchObjectLock, to make main thread wait until all worker threads have completed. A common use case this example can solve is a server that must start three subsystems (database, cache, network listener) before it can accept traffic. You can use a latch to make the main thread wait until all subsystems have signaled they’re ready.

---
+++ general/src/main/java/SimpleCountDownLatch/CountDownLatchExample.java
@@ -0,0 +1,33 @@
+package SimpleCountDownLatch;
+
+public class CountDownLatchExample {
+    public static void main(String[] args) throws InterruptedException {
+        int workerCount = 3;
+        SimpleCountDownLatchObjectLock latch = new SimpleCountDownLatchObjectLock(workerCount);
+
+        for (int i = 1; i <= workerCount; i++) {
+            Thread worker = new Thread(new Worker(latch, i));
+            worker.start();
+        }
+
+        System.out.println("Main thread waiting for workers to finish...");
+        latch.await();  // Blocks until count reaches 0
+        System.out.println("All workers finished. Main thread continues!");
+    }
+
+    record Worker(SimpleCountDownLatchObjectLock latch, int id) implements Runnable {
+
+        @Override
+            public void run() {
+                try {
+                    System.out.println("Worker " + id + " is doing work...");
+                    Thread.sleep(1000 + id * 500L); // Simulate work
+                    System.out.println("Worker " + id + " finished.");
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    latch.countDown(); // Decrement the count
+                }
+            }
+        }
+}
