1bb8545f5dfcfcdee868ff73ee4ce7fbe359b58c
Paramjot Singh <Paramjotdhiman5@gmail.com>
2025-09-01 21:24:58 +0530

    feat: general usage of AtomicReference, CAS

---
+++ general/src/main/java/atomic/reference/Main.java
@@ -0,0 +1,18 @@
+package atomic.reference;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Main {
+    public static void main(String[] args) {
+        String oldName = "Bruce Wayne";
+        String newName = "Batman";
+        AtomicReference<String > atomicReference = new AtomicReference<>(oldName);
+
+        if(atomicReference.compareAndSet(oldName, newName)) {
+            System.out.println("Name updated to: " + atomicReference.get());
+        } else {
+            System.out.println("Update failed. Current name: " + atomicReference.get());
+        }
+
+    }
+}
