79fa020105e819aec4144c95fb162702522241c7
Paramjot Singh <Paramjotdhiman5@gmail.com>
2025-09-02 20:42:45 +0530

    feat: Impliment LockFree (Treiber) Stack, following fundamentals from pptimistic concurrency programming. Consume CAS from AtomicReference types.

--- general/src/main/java/atomic/reference/Main.java
+++ general/src/main/java/atomic/reference/Main.java
@@ -3,15 +3,17 @@ package atomic.reference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.LockSupport;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        StandardStack<Integer> stack = new StandardStack<>();
+        Stack<Integer> stack = new LockFreeStack<>();
         Random random = new Random();
 
-        List<Thread> threads = getThreads(stack, random);
-
-        for (Thread thread : threads) {
+        for (Thread thread : getThreads(stack, random)) {
             thread.start();
         }
 
@@ -20,7 +22,7 @@ public class Main {
         System.out.printf("%,d operations were performed in 10 seconds %n", stack.getCounter());
     }
 
-    private static List<Thread> getThreads(StandardStack<Integer> stack, Random random) {
+    private static List<Thread> getThreads(Stack<Integer> stack, Random random) {
         List<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < 2; i++) {
@@ -43,9 +45,63 @@ public class Main {
         return threads;
     }
 
-    public static class StandardStack<T>{
+    // Classic Treiber stack, also called optimistic concurrency.
+    public static class LockFreeStack<T> implements Stack<T> {
+        private final AtomicReference<StackNode<T>> head = new AtomicReference<>();
+        private final AtomicLong stepCounter = new AtomicLong(0);
+
+        public void push(T value) {
+            StackNode<T> newHeadNode = new StackNode<>(value);
+
+            while (true) {
+                StackNode<T> currentHeadNode = head.get();
+                newHeadNode.next = currentHeadNode;
+                if (head.compareAndSet(currentHeadNode, newHeadNode)) {
+                    break;
+                } else {
+                    LockSupport.parkNanos(1);
+                }
+            }
+            stepCounter.incrementAndGet();
+        }
+
+        public T pop() {
+            StackNode<T> currentHeadNode = head.get();
+            StackNode<T> newHeadNode;
+
+            while (currentHeadNode != null) {
+                newHeadNode = currentHeadNode.next;
+                if (head.compareAndSet(currentHeadNode, newHeadNode)) {
+                    break;
+                } else {
+                    LockSupport.parkNanos(1);
+                    currentHeadNode = head.get();
+                }
+            }
+            stepCounter.incrementAndGet();
+            return currentHeadNode != null ? currentHeadNode.value : null;
+        }
+
+        public Long getCounter() {
+            return stepCounter.get();
+        }
+
+
+        private static int backoff(int i) {
+            if (i < 10) {
+                Thread.onSpinWait(); // JDK 9+
+                return i + 1;
+            } else {
+                // cap the backoff; tune for your workload
+                LockSupport.parkNanos(50);
+                return 0;
+            }
+        }
+    }
+
+    public static class StandardStack<T> implements Stack<T> {
         private StackNode<T> head;
-        private int stepCounter = 0;
+        private Long stepCounter = 0L;
 
         public synchronized void push(T t){
             StackNode<T> node = new StackNode<>(t);
@@ -65,9 +121,15 @@ public class Main {
             return value;
         }
 
-        public int getCounter(){
+        public Long getCounter(){
             return stepCounter;
         }
+    }
+
+    public interface Stack<T>{
+        void push(T value);
+        T pop();
+        Long getCounter();
     }
 
     private static class StackNode<T>{
