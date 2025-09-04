103a9dfc56a28082aaf79f231dd90edab803b7f1
Paramjot Singh <Paramjotdhiman5@gmail.com>
2025-09-04 22:57:30 +0530

    chore: Idealy, this change should not have any impact, but this significanlty degraded the app performance. This changes required 2136 ms to complete 1000 number tasks, almost double of what required earlier. This is because of the number of context switches is more then earlier.

--- iobound/src/Main.java
+++ iobound/src/Main.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ThreadFactory;
 
 public class Main {
 
-    private static final int NUMBER_OF_TASKS = 20000;
+    private static final int NUMBER_OF_TASKS = 1000;
 
     public static void main(String[] args) {
         System.out.println("Starting IO-bound tasks...");
@@ -25,7 +25,9 @@ public class Main {
     private static void blockingIOOperation(){
         System.out.println("Executing a blocking IO task from thread: " + Thread.currentThread().getName());
         try {
-            Thread.sleep(1000); // Simulate blocking IO with sleep
+           for(int i = 0; i < 100; i++){
+               Thread.sleep(10);
+           }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
