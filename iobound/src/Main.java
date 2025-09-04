70aea04f9ff2624ea0ae5c933d0492283b7a79ca
Paramjot Singh <Paramjotdhiman5@gmail.com>
2025-09-04 21:45:20 +0530

    feat: Define a cached poll of threads from ExecutorService. Cached pool is able tp complete 1000 tasks within ~1000 ms (=1044 ms), where each task takes 1000 ms, which means Executor have spins up 1000 threads and delegates all tasks per thread, which we can see logs also. This is the behaviour of 'newCachedThreadPool' that spins up as many threads as many tasks are submitted to the pool. This behvaiour can be chalangeing in non-monitored systems, where ludicurosily large number of tasks can be submitted to the pool and we might enter into a situation of no more threads can be scheduled from OS or machine.

---
+++ iobound/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+public class Main {
+
+    private static final int NUMBER_OF_TASKS = 1000;
+
+    public static void main(String[] args) {
+        System.out.println("Starting IO-bound tasks...");
+        long startTime = System.currentTimeMillis();
+        performTasks();
+        long endTime = System.currentTimeMillis();
+        System.out.println("All tasks completed in " + (endTime - startTime) + " ms");
+    }
+
+    private static void performTasks(){
+        try(ExecutorService executorService = Executors.newCachedThreadPool()){
+            for (int i = 0; i < NUMBER_OF_TASKS; i++) {
+                executorService.submit(Main::blockingIOOperation);
+            }
+        }
+    }
+
+    private static void blockingIOOperation(){
+        System.out.println("Executing a blocking IO task from thread: " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000); // Simulate blocking IO with sleep
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
