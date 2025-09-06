965672e524842b9622bc12607cc2fe8966ad3ef2
Paramjot Singh <Paramjotdhiman5@gmail.com>
2025-09-06 22:45:12 +0530

    feat: At this change, we only have 12 worker threads, however we were able to run 1000 separate tasks. Format VirtualThread[#1016]/runnable@ForkJoinPool-1-worker-12: [ThreadId]/runnable@WorkerThreadPoll-Name-WorkerThread-Id.

--- general/src/main/java/virtualthreads/Main.java
+++ general/src/main/java/virtualthreads/Main.java
@@ -1,12 +1,29 @@
 package virtualthreads;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    private final int NUMBER_OF_VIRTUAL_THREADS = 1000;
+
     public static void main(String[] args) throws InterruptedException {
         Runnable runnable = () -> System.out.println("Hello from " + Thread.currentThread());
 
-        Thread thread = Thread.ofPlatform().unstarted(runnable);
 
-        thread.start();
-        thread.join();
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            Thread thread = Thread.ofVirtual().unstarted(runnable);
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
     }
 }
