ca23b8eeff776a8dea4f19cb4b19dadef6202067
Paramjot Singh <Paramjotdhiman5@gmail.com>
2025-09-09 22:32:45 +0530

    feat: Platform threads takes 15675 ms to complete 10000 tasks on fixed thread size poll of 1000 threads. This much wait is because of context switches after each 10ms for 100 times of each task. Mounting/Unmounting contexts/Stack/IP from platform thread is expensive and time consuming. This problem with context switching or Thrashing is resolved with virtual threads, where the heavy lifting of context switching is handled by the JVM.

---
+++ general/src/main/java/virtualthreads/VirtualThreadPerformance.java
@@ -0,0 +1,36 @@
+package virtualthreads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class VirtualThreadPerformance {
+
+    private static final int NUMBER_OF_TASKS = 10000;
+
+    public static void main(String[] args) {
+        System.out.println("Starting IO-bound tasks...");
+        long startTime = System.currentTimeMillis();
+        performTasks();
+        long endTime = System.currentTimeMillis();
+        System.out.println("All tasks completed in " + (endTime - startTime) + " ms");
+    }
+
+    private static void performTasks(){
+        try(ExecutorService executorService = Executors.newFixedThreadPool(1000)){
+            for (int i = 0; i < NUMBER_OF_TASKS; i++) {
+                executorService.submit(VirtualThreadPerformance::blockingIOOperation);
+            }
+        }
+    }
+
+    private static void blockingIOOperation(){
+        System.out.println("Executing a blocking IO task from thread: " + Thread.currentThread().getName());
+        try {
+            for(int i = 0; i < 100; i++){
+                Thread.sleep(10);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
