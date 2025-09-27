af6a66443b11eef71382ba38be6cc88e81e763f2
Paramjot Singh <Paramjotdhiman5@gmail.com>
2025-09-28 02:05:36 +0530

    feat: Impliment a synchronized Stack, let couple of producer and consumer threads to opreate on to stack and calculate how many opreations can a synchronized stack can perform. Mac M4 Pro (169,058,573)

--- general/src/main/java/atomic/reference/Main.java
+++ general/src/main/java/atomic/reference/Main.java
@@ -1,19 +1,81 @@
 package atomic.reference;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        String oldName = "Bruce Wayne";
-        String newName = "Batman";
-        AtomicReference<String > atomicReference = new AtomicReference<>(oldName);
+    public static void main(String[] args) throws InterruptedException {
+        StandardStack<Integer> stack = new StandardStack<>();
+        Random random = new Random();
 
-        atomicReference.set(newName);
-        if(atomicReference.compareAndSet(oldName, newName)) {
-            System.out.println("Name updated to: " + atomicReference.get());
-        } else {
-            System.out.println("Update failed. Current name: " + atomicReference.get());
+        List<Thread> threads = getThreads(stack, random);
+
+        for (Thread thread : threads) {
+            thread.start();
         }
 
+        Thread.sleep(10000);
+
+        System.out.printf("%,d operations were performed in 10 seconds %n", stack.getCounter());
+    }
+
+    private static List<Thread> getThreads(StandardStack<Integer> stack, Random random) {
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+            Thread producerThread = new Thread(() -> {
+                while (true) {
+                    stack.push(random.nextInt());
+                }
+            });
+            producerThread.setDaemon(true);
+            threads.add(producerThread);
+
+            Thread consumerThread = new Thread(() -> {
+                while (true) {
+                    stack.pop();
+                }
+            });
+            consumerThread.setDaemon(true);
+            threads.add(consumerThread);
+        }
+        return threads;
+    }
+
+    public static class StandardStack<T>{
+        private StackNode<T> head;
+        private int stepCounter = 0;
+
+        public synchronized void push(T t){
+            StackNode<T> node = new StackNode<>(t);
+            node.next = head;
+            head = node;
+            stepCounter++;
+        }
+
+        public synchronized T pop(){
+            if(head == null){
+                return null;
+            }
+
+            T value = head.value;
+            head = head.next;
+            stepCounter++;
+            return value;
+        }
+
+        public int getCounter(){
+            return stepCounter;
+        }
+    }
+
+    private static class StackNode<T>{
+        public T value;
+        public StackNode<T> next;
+
+        public StackNode(T value) {
+            this.value = value;
+        }
     }
 }
